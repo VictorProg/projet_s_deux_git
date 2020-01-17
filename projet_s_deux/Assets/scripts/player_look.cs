957b3132af066a23295ebee57e9b541178ade82c
Antoine6quarts <antoine.sicard@live.fr>
2020-01-17 21:33:06 +0100

    debut_creation_personnage

---
+++ projet_s_deux/Assets/scripts/player_look.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class player_look : MonoBehaviour
+{
+    public float mouseSensitivity = 100f;
+    public Transform player;
+    float lookUpAndDown = 0f;
+    float gravity = 4.19f;
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void Update()
+    {
+        //récuperer positions sourie
+        float viewX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float viewY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+        
+        //regarder gauche droite
+        player.Rotate(Vector3.up * viewX); 
+        
+        //regarder haut bas
+        lookUpAndDown -= viewY; //définir l'angle de mouvement caméra
+        lookUpAndDown = Mathf.Clamp(lookUpAndDown, -90f, 90); //vérouiller vision verticale entre 90° et -90° (position de départ : 0°)
+        transform.localRotation = Quaternion.Euler(lookUpAndDown, 0f, 0f); //tourner la caméra verticalement de lookUpAndDown°
+
+        //appliquer la gravité au joueur
+
+
+        
+    }
+}
---
+++ projet_s_deux/Assets/scripts/player_movement.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class player_movement : MonoBehaviour
+{
+    public CharacterController character;
+    public float speed = 12f;
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal"); //récupérer l'orientation verticale de la caméra
+        float z = Input.GetAxis("Vertical"); //récupérer l'orientation horizontale de la caméra
+
+        //déterminer dans quelle direction se déplacer
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        //se déplacer
+        character.Move(move * speed * Time.deltaTime);
+    }
+}
